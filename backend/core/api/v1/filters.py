628efe957392b728dd25064ba4c4a318f31db2f6
Lam <shun_min@hotmail.com>
2024-08-18 22:27:51 +0800

    update django filters

--- backend/core/api/v1/filters.py
+++ backend/core/api/v1/filters.py
@@ -1,30 +1,29 @@
+import django_filters
 from django.db.models import QuerySet
-from django_filters import rest_framework as filters
 
 from core.models import (
     Song,
 )
 
 
-class SongFilter(filters.FilterSet):
-    # artist__name = filters.CharFilter(
+class SongFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
         # method = "getSongsByArtist", 
         # label="Group By Artist",
-    #     lookup_expr="icontains",
-    # )
+        lookup_expr='iexact',
+    )
+    language__value = django_filters.CharFilter(
+        field_name='language',
+        lookup_expr='contains',
+    )
     class Meta:
         model = Song
         fields = [
-            'artist__name', 
-            'language__value',
+            'language',
+            'name',
         ]
 
-    def getSongsByArtist(self, queryset: QuerySet, name, value: str):
-        song_ids = Song.objects.filter(
-            artist=value,
-        ).value_list("song_id", flat=True)
-        if song_ids:
-            return queryset.filter(id__in=song_ids)
-
-        return queryset.none()
+    def getSongsByArtist(self, queryset: QuerySet, language: str):
+        # return queryset.filter(artist__name__in=artist_name)
+        return queryset.filter(language__value__in=language)
         
