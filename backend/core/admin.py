bb91ec3338264bf570d6be89c294260c2ab6de92
Lam <shun_min@hotmail.com>
2025-02-16 16:22:21 +0800

    - fix playlist item serializer, value not assigned when creating instance - edit admin list display

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -15,6 +15,28 @@ admin.site.register(Album)
 admin.site.register(Artist)
 admin.site.register(Genre)
 admin.site.register(Language)
-admin.site.register(Playlist)
-admin.site.register(PlaylistItem)
-admin.site.register(Song)
+
+class PlaylistFields(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+admin.site.register(Playlist, PlaylistFields)
+
+class PlaylistItemFields(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "playlist",
+        "song",
+    )
+admin.site.register(PlaylistItem, PlaylistItemFields)
+
+class SongFields(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "language",
+        "album",
+    )
+admin.site.register(Song, SongFields)
