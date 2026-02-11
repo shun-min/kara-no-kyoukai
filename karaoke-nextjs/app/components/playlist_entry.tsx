7862b1f0d5cb56154fb3ed088eee958ba3c86006
Lam <shun_min@hotmail.com>
2026-02-11 22:35:40 +0800

    Reuse same tab when switching songs

--- karaoke-nextjs/app/components/playlist_entry.tsx
+++ karaoke-nextjs/app/components/playlist_entry.tsx
@@ -25,23 +25,17 @@ export default function PlaylistEntry({
   function playSong(src_path: string) {
     localStorage.setItem("currSong", src_path);
     console.log("playing: ", src_path);
-    // router.push("/");
+    window.open("/player", "songTab");
   }
 
   return (
     <div className="flex gap-x-2 m-2 items-center">
-      {/* <button
+      <button
         onClick={() => src_path && playSong(src_path)}
         className="flex-1 text-left px-3 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded transition-colors"
       >
         {order}. {songName}
-      </button> */}
-      <Link
-        href="/player" target="_blank" rel="noopener noreferrer"
-        onClick={() => src_path && playSong(src_path)}
-      >
-        {order}. {songName}
-      </Link>
+      </button>
       <button
         onClick={() => onRemove(id)}
         className="px-3 py-2 bg-red-500 hover:bg-red-600 text-white rounded transition-colors"
